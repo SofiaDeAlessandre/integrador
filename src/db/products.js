b86bea7182fb7d60dd16b3055e8e5b0303c61ac3
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-01-26 19:08:58 -0300

    add cards dynamically

--- src/db/products.js
+++ src/db/products.js
@@ -2,13 +2,14 @@ const products = [
    {
     id: 1,
     name: "Sansevieria",
-    img: "img/sansevieria",
+    img: "imgs/sansevieria.jpg",
     description: "También conocida como lengua de tigre o lengua de suegra, es otra de las plantas de interior de moda y perfecta para principiantes.Es fácil de cuidar, duradera, necesita poco riego y luz abundante. Además, es resistente a plagas y a temperaturas extremas. Si la incluyes en tu casa, te ayudará a limpiar el aire y eliminar sustancias toxicas. Es perfecta para colocar en cualquier rincón de la casa o sobre un mueble.",
     price: 222.20
    },
    {
     id: 2,
     name: "Cintas",
+     img: "imgs/cintas.jpg",
     description: "también conocidas como lazo de amor, son una de las plantas más duraderas y fáciles de cuidar, por lo que te las recomendamos si las plantas no son lo tuyo o eres principiante. Para cuidarlas, procura proporcionarles luz, pero sin exponerla directamente, y regarla una vez a la semana (2 ó 3 en meses más calurosos). Podrás obtener fácilmente esquejes de esta planta a través de los “hijos” que aparecen en sus tallos.",
     price: 5533
    }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,42 @@
-import products from './db/products'
-import './sass/main.scss'
+import products from "./db/products";
+import "./sass/main.scss";
 
-console.log(products)
+const containerProducts = document.getElementById("container-products");
+
+let html = "";
+containerProducts.innerHTML = html;
+
+const start = () => {
+  products.forEach((prod) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const article = document.createElement("article");
+    article.classList.add("card__article");
+    card.appendChild(article);
+    const cardImageContainer = document.createElement("div");
+    cardImageContainer.classList.add("card__image-container");
+    const image = document.createElement("img");
+    image.classList.add("card__image");
+    image.src = prod.img;
+    image.alt = prod.name;
+    cardImageContainer.appendChild(image);
+    article.appendChild(cardImageContainer);
+    const cardContent = document.createElement("div");
+    const h2Title = document.createElement("h2");
+    h2Title.classList.add("card__heading");
+    h2Title.textContent = prod.name;
+    const cardDescription = document.createElement("div");
+    cardDescription.classList.add("card__description");
+    const p = document.createElement("p");
+    p.textContent = prod.description;
+
+    cardContent.appendChild(h2Title);
+    cardDescription.appendChild(p);
+    cardContent.appendChild(cardDescription);
+    article.appendChild(cardContent);
+    containerProducts.appendChild(card);
+  });
+};
+
+window.addEventListener("DOMContentLoaded", start);
 
