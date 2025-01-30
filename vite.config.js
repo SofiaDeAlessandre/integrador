b43408499c3b8899f8c5a3653b20c7cf56d72274
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-01-30 14:42:21 -0300

    add page 'nosotros'

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default {
     },
     build: {
         rollupOptions: {
-            input: resolve(__dirname, 'index.html')
+            input: resolve(__dirname, 'index.html'),
+            nosotros: resolve(__dirname, 'pages/nosotros/index.html'),
         }
     }
 }
