b01dbd64f88eea5b6491799779c24b9b082217ef
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-02-10 19:10:03 -0300

    fix 'vite.config.js'

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { resolve } from "path";
 
 export default {
-    css: {
-      devSourcemap: true
-    },
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'index.html'),
-          nosotros: resolve(__dirname, 'pages/nosotros/nosotros.html'),
-          contacto: resolve(__dirname, 'pages/contacto/contacto.html')
-        }
+  css: {
+    devSourcemap: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nosotros: resolve(__dirname, 'src/pages/nosotros/nosotros.html'),
+        contacto: resolve(__dirname, 'src/pages/contacto/contacto.html')
       }
     }
   }
+}
