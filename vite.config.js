7df376f25ad66baa855025cdb8678feda025235d
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-02-10 17:47:07 -0300

    fix routes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { resolve } from "path";
 
 export default {
-  css: {
-    devSourcemap: true,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nosotros: resolve(__dirname, "pages/nosotros/index.html"),
-        contacto: resolve(__dirname, "pages/contacto/index.html"),
-      },
+    css: {
+      devSourcemap: true
     },
-  },
-};
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          nosotros: resolve(__dirname, 'pages/nosotros/index.html'),
+          contacto: resolve(__dirname, 'pages/contacto/index.html')
+        }
+      }
+    }
+  }
